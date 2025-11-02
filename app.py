bec7e339cb9fe774ffa409075f13f78d8731a7ef
fionaleongworkspace <fionaleongworkspace@gmail.com>
2025-11-02 23:02:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ div.stButton > button { border-radius: 12px; font-weight: 600; padding: .6rem 1r
 TITLE = "Creativity ↔ Analytics Quiz"
 INTRO = (
     "Answer 10 quick A/B choices. There’s no “right” answer — "
-    "This is just to map out your default problem-solving style. Have fun!"
+    "this is just to find out your default problem-solving style."
 )
 
 QUESTIONS = [
